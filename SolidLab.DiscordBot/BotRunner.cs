512ade0ac54c2674e1d64439aa01ebd65f51e4a7
rnegrin90 <rnegrin1990@gmail.com>
2016-10-13 03:20:25 +0100

    Started sound service

--- SolidLab.DiscordBot/BotRunner.cs
+++ SolidLab.DiscordBot/BotRunner.cs
@@ -2,13 +2,16 @@
 using System.Configuration;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Audio;
 using Discord.Commands;
+using SolidLab.DiscordBot.Sound;
 
 namespace SolidLab.DiscordBot
 {
     public class BotRunner : IRunner
     {
         private readonly DiscordClient _client;
+        private SingleSoundsService _singleSoundsService;
 
         public BotRunner(DiscordClient client)
         {
@@ -24,6 +27,17 @@ namespace SolidLab.DiscordBot
                 c.PrefixChar = ConfigurationManager.AppSettings["DiscordBot:Prefix"].ToCharArray()[0];
                 c.HelpMode = HelpMode.Public;
             });
+
+            _client.UsingAudio(x => // Opens an AudioConfigBuilder so we can configure our AudioService
+            {
+                x.Mode = AudioMode.Outgoing; // Tells the AudioService that we will only be sending audio
+            });
+
+
+            _singleSoundsService = new SingleSoundsService(_client.GetService<AudioService>());
+            var cmdService = _client.GetService<CommandService>();
+
+            _singleSoundsService.SetUpCommands(cmdService);
 
             return _client.Connect(ConfigurationManager.AppSettings["DiscordBot:Token"], TokenType.Bot);
         }
---
+++ SolidLab.DiscordBot/Sound/SingleSoundsService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.Audio;
+using Discord.Commands;
+using NAudio.Wave;
+
+namespace SolidLab.DiscordBot.Sound
+{
+    public class SingleSoundsService
+    {
+        private ISoundsRepository _soundsRepo;
+        private readonly AudioService _audioService;
+
+        public SingleSoundsService(AudioService audioService)
+        {
+            _audioService = audioService;
+        }
+
+        public void SetUpCommands (CommandService cmdService)
+        {
+            cmdService.CreateCommand("sound")
+                .Parameter("SoundName")
+                .Description("Play a sound (If found!)")
+                .Do(async e => await PlaySound(e, e.GetArg("SoundName")));
+        }
+
+        private async Task PlaySound(CommandEventArgs ev, string soundName)
+        {
+            var userVoiceChannel = ev.User.VoiceChannel;
+
+            if (userVoiceChannel == null)
+            {
+                await ev.Channel.SendMessage("You must be in a voice channel to use this command!");
+                return;
+            }
+
+            try
+            {
+                var vClient = await _audioService.Join(userVoiceChannel);
+                var OutFormat = new WaveFormat(48000, 16, _audioService.Config.Channels); // Create a new Output Format, using the spec that Discord will accept, and with the number of channels that our client supports.
+                using (var MP3Reader = new Mp3FileReader("./TempResources/AIRHORN.mp3")) // Create a new Disposable MP3FileReader, to read audio from the filePath parameter
+                using (var resampler = new MediaFoundationResampler(MP3Reader, OutFormat)) // Create a Disposable Resampler, which will convert the read MP3 data to PCM, using our Output Format
+                {
+                    resampler.ResamplerQuality = 60; // Set the quality of the resampler to 60, the highest quality
+                    int blockSize = OutFormat.AverageBytesPerSecond / 50; // Establish the size of our AudioBuffer
+                    byte[] buffer = new byte[blockSize];
+                    int byteCount;
+
+                    while ((byteCount = resampler.Read(buffer, 0, blockSize)) > 0) // Read audio into our buffer, and keep a loop open while data is present
+                    {
+                        if (byteCount < blockSize)
+                        {
+                            // Incomplete Frame
+                            for (int i = byteCount; i < blockSize; i++)
+                                buffer[i] = 0;
+                        }
+                        vClient.Send(buffer, 0, blockSize); // Send the buffer to Discord
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    public interface ISoundsRepository
+    {
+        List<string> GetAvailableSounds();
+
+    }
+}
